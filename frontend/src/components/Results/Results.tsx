02ff4ce2a603edb5ff90a0d77d817e3d74c73bde
Mindaugas Dirma <mindaugas.dirma.ext@zenjob.com>
2022-12-20 15:43:25 +0200

    added no results

--- frontend/src/components/Results/Results.tsx
+++ frontend/src/components/Results/Results.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Alert, Container, Row } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
 import { Data, Word } from "../../models/response";
 
@@ -8,38 +8,44 @@ interface ResultsProps {
 }
 
 const Results: React.FC<ResultsProps> = ({ data }) => {
+  if (data.length === 0) {
+    return (
+      <Container fluid className="col-md-6 mt-md-3">
+        <Row align="center">
+          <Alert variant="warning">Results are empty</Alert>
+        </Row>
+      </Container>
+    );
+  }
+
   return (
-    <Container>
-        {data.map((group: Data) => {
-          return (
-            <div key={group.name}>
-              <div className="row">
-                <div className="col-md-8 offset-md-2">
-                  <br />
-                  <h4>{group.name}</h4>
-                  <Table striped bordered hover size="sm" responsive="md">
-                    <thead>
-                      <tr>
-                        <th className="col-md-4">Word</th>
-                        <th className="col-md-4">Count</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {group.words.map((result: Word) => {
-                        return (
-                          <tr key={result.word}>
-                            <td className="col-md-4">{result.word}</td>
-                            <td className="col-md-4">{result.count}</td>
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </Table>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+    <Container fluid className="mb-md-5">
+      {data.map((group: Data) => {
+        return (
+          <div key={group.name}>
+            <br />
+            <h4>{group.name}</h4>
+            <Table striped bordered hover size="sm" responsive="md">
+              <thead>
+                <tr>
+                  <th className="col-md-4">Word</th>
+                  <th className="col-md-4">Count</th>
+                </tr>
+              </thead>
+              <tbody>
+                {group.words.map((result: Word) => {
+                  return (
+                    <tr key={result.word}>
+                      <td className="col-md-4">{result.word}</td>
+                      <td className="col-md-4">{result.count}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </div>
+        );
+      })}
     </Container>
   );
 };
